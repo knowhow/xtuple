ac75ad0c72be8eeb7122aa864d05a718c00486c7
Ernad Husremovic <hernad@bring.out.ba>
2011-10-15 14:11:43 +0200

    QMessageBox script

--- scriptapi/knowhow_utils.cpp
+++ scriptapi/knowhow_utils.cpp
@@ -1,10 +1,25 @@
-#include "knohow_utils.h"
+#include "knowhow_utils.h"
+
+/* dzon
+void setupQUrlProto(QScriptEngine *engine)
+{
+  if (DEBUG) qDebug("setupQUrlProto entered");
+
+  QScriptValue urlproto = engine->newQObject(new QUrlProto(engine));
+  engine->setDefaultPrototype(qMetaTypeId<QUrl*>(), urlproto);
+
+  QScriptValue urlConstructor = engine->newFunction(constructQUrl, urlproto);
+  engine->globalObject().setProperty("QUrl", urlConstructor);
+}
+*/
+
 
 void setupQMessageBox(QScriptEngine *engine)
 {
   //qScriptRegisterMetaType(engine, XTreeWidgetItemtoScriptValue,     XTreeWidgetItemfromScriptValue);
   //qScriptRegisterMetaType(engine, XTreeWidgetItemListtoScriptValue, XTreeWidgetItemListfromScriptValue);
 
-  QScriptValue constructor = engine->newFunction(qscript_call);
+  QScriptValue constructor = engine->newQObject(new Wrapper_QMessageBox(engine));
+  //engine->setDefaultPrototype(qMetaTypeId<Wrapper_QMessageBox*>(), constructor);
   engine->globalObject().setProperty("QMessageBox", constructor, QScriptValue::ReadOnly | QScriptValue::Undeletable);
 }
--- scriptapi/knowhow_utils.h
+++ scriptapi/knowhow_utils.h
@@ -1,9 +1,15 @@
 #include "include.h"
+#include <QMessageBox>
+
+// http://tech-foo.blogspot.com/2008/05/qtscript-exposing-c-classes-part-two.html
 
 class Wrapper_QMessageBox: public QMessageBox, protected QScriptable
 {
  Q_OBJECT
  public:
+ 
+  Wrapper_QMessageBox(QScriptEngine *engine)
+ : QMessageBox(0) {}
  
  Wrapper_QMessageBox(QWidget *parent =0)
  : QMessageBox(parent) {}
